89c74ffa683082fb5fd337fc3f99431cd67c13ac
yashasvi <yashasvi.tiwari.4648@gmail.com>
2023-09-19 11:49:29 +0545

    inital setup of remix

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
+  Form,
   Links,
   LiveReload,
   Meta,
@@ -8,9 +9,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "~/tailwind.css";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export default function App() {
@@ -23,10 +25,35 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search" method="post">
+              <input
+                type="text"
+                aria-label="Search Contacts"
+                id="q"
+                name="q"
+                placeholder="Search"
+              />
+              <div aria-hidden hidden={true} id="search-spinner"></div>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="/contact/1"> Yashasvi Tiwari</a>
+              </li>
+              <li>
+                <a href="/contact/2">Saurab Bikram Sen</a>
+              </li>
+            </ul>
+          </nav>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </div>
       </body>
     </html>
   );
